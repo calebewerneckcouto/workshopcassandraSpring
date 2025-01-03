cf3fcebef1eaffce7479bc7c7d0f6120da96ac81
calebewerneckcouto <calebewerneck@hotmail.com>
2025-01-03 01:24:30 -0300

    Buscando todos os  produtos quando o departamento nao for informado

--- src/main/java/com/devsuperior/workshopcassandra/services/ProductService.java
+++ src/main/java/com/devsuperior/workshopcassandra/services/ProductService.java
@@ -12,30 +12,35 @@ import com.devsuperior.workshopcassandra.model.dto.ProductDTO;
 import com.devsuperior.workshopcassandra.model.entities.Product;
 import com.devsuperior.workshopcassandra.repositories.ProductRepository;
 import com.devsuperior.workshopcassandra.services.exceptions.ResourceNotFoundException;
+
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	private ProductRepository repository;
-	
-	
-	public ProductDTO findById(UUID  id) {
+
+	public ProductDTO findById(UUID id) {
 		Product entity = getById(id);
 		return new ProductDTO(entity);
 	}
 
 	private Product getById(UUID id) {
-		
+
 		Optional<Product> result = repository.findById(id);
-		return result.orElseThrow(()-> new ResourceNotFoundException("Id não encontrado"));
+		return result.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado"));
+
+	}
+
+	public List<ProductDTO> findByDepartment(String department) {
+		List<Product> list;
+		if ("".equals(department)) {
+			list = repository.findAll();
+		}else {
+			list = repository.findByDepartament(department);
+		}
+
 		
+		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
 	}
-	
-	
-	public List<ProductDTO>findByDepartment(String department){
-		List<Product> list = repository.findByDepartament(department);
-		return list.stream().map(x-> new ProductDTO(x)).collect(Collectors.toList());
-	}
-	
 
 }
