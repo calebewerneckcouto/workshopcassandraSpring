9e4278b997719cf450e08562c82b7a3d6e17a8fc
calebewerneckcouto <calebewerneck@hotmail.com>
2025-01-03 00:03:38 -0300

    Departament id

--- src/main/java/com/devsuperior/workshopcassandra/controllers/DepartamentController.java
+++ src/main/java/com/devsuperior/workshopcassandra/controllers/DepartamentController.java
@@ -1,14 +1,17 @@
 package com.devsuperior.workshopcassandra.controllers;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.workshopcassandra.model.dto.DepartamentDTO;
+import com.devsuperior.workshopcassandra.model.entities.Departament;
 import com.devsuperior.workshopcassandra.services.DepartamentService;
 
 @RestController
@@ -21,6 +24,13 @@ public class DepartamentController {
 	public ResponseEntity<List<DepartamentDTO>>findAll(){
 		List<DepartamentDTO> list = service.findAll();
 		return ResponseEntity.ok(list);
+	}
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<DepartamentDTO>findById(@PathVariable UUID id){
+		DepartamentDTO entity = service.findById(id);
+		return ResponseEntity.ok(entity);
 	}
 
 }
--- src/main/java/com/devsuperior/workshopcassandra/services/DepartamentService.java
+++ src/main/java/com/devsuperior/workshopcassandra/services/DepartamentService.java
@@ -1,6 +1,8 @@
 package com.devsuperior.workshopcassandra.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.workshopcassandra.model.dto.DepartamentDTO;
 import com.devsuperior.workshopcassandra.model.entities.Departament;
 import com.devsuperior.workshopcassandra.repositories.DepartamentRepository;
+import com.devsuperior.workshopcassandra.services.exceptions.ResourceNotFoundException;
 @Service
 public class DepartamentService {
 	
@@ -18,6 +21,13 @@ public class DepartamentService {
 	public List<DepartamentDTO> findAll() {
 		List<Departament> list = repository.findAll();
 		return list.stream().map(x-> new DepartamentDTO(x)).collect(Collectors.toList());
+	}
+	
+	
+	public DepartamentDTO findById(UUID  id) {
+		Optional<Departament> result = repository.findById(id);
+		Departament entity = result.orElseThrow(()-> new ResourceNotFoundException("Id não encontrado"));
+		return new DepartamentDTO(entity);
 	}
 
 }
